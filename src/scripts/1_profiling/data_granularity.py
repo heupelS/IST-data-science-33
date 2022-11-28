97403b3ab3b0288f623e1d146a29e0eeb93466ff
Simeon <simeon@heupel.de>
2022-11-28 21:19:26 +0000

    fixed typo

---
+++ src/scripts/1_profiling/data_granularity.py
@@ -0,0 +1,34 @@
+
+import sys, os
+
+sys.path.append( os.path.join(os.path.dirname(__file__), '..', '..','utils') )
+from general_utils import get_plot_folder_path
+from load_data import read_data
+
+from ds_charts import get_variable_types, choose_grid, HEIGHT
+from matplotlib.pyplot import subplots, savefig, show
+
+def data_granuality(data, name):
+
+    variables = get_variable_types(data)['Numeric']
+    if [] == variables:
+        raise ValueError('There are no numeric variables.')
+
+    rows, cols = choose_grid(len(variables))
+    fig, axs = subplots(rows, cols, figsize=(cols*HEIGHT* 1.5, rows*HEIGHT * 1.5), squeeze=False)
+    i, j = 0, 0
+
+    for n in range(len(variables)):
+        axs[i, j].set_title('Histogram for %s'%variables[n])
+        axs[i, j].set_xlabel(variables[n])
+        axs[i, j].set_ylabel('nr records')
+        axs[i, j].hist(data[variables[n]].values, bins=100)
+        i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+
+    savefig( os.path.join(get_plot_folder_path(), name) )
+    show()
+
+if __name__ == "__main__":
+    data_diabetic, data_drought = read_data()
+
+    data_granuality(data_diabetic, 'diabetic_data_granuality')
