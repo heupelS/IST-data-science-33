4b44eefc76a8a28accd1d2f5a42e848038f54d26
Simeon <simeon@heupel.de>
2022-11-29 02:32:57 +0000

    sparsity added

--- src/scripts/1_profiling/data_sparsity.py
+++ src/scripts/1_profiling/data_sparsity.py
@@ -3,12 +3,12 @@
 import sys, os
 from pandas import read_csv
 from pandas.plotting import register_matplotlib_converters
-from matplotlib.pyplot import subplots, savefig, show, title
-from ds_charts import get_variable_types, HEIGHT
+from matplotlib.pyplot import subplots, savefig, show, title, figure
 from seaborn import heatmap
 
 sys.path.append( os.path.join(os.path.dirname(__file__), '..', '..','utils') )
 
+from ds_charts import get_variable_types, HEIGHT
 from general_utils import get_plot_folder_path
 from load_data import read_data
 
@@ -18,9 +18,13 @@ SHOW_PLOTS = False
 ###########################################
 
 def plot_scatter(data, name):
+
     register_matplotlib_converters()
 
-    vars = get_variable_types(data)
+    vars = get_variable_types(data)['Numeric']
+    #filter empty values
+    #vars = { k:v for (k,v) in zip(vars.keys(), vars.values()) if v != [] and k != 'Date' and k != 'Binary'} #need that later maybe
+
     if [] == vars:
         raise ValueError('There are no numeric variables.')
 
@@ -35,12 +39,17 @@ def plot_scatter(data, name):
             axs[i, j-1].set_ylabel(var2)
             axs[i, j-1].scatter(data[var1], data[var2])
 
-    savefig( os.path.join(get_plot_folder_path(), f'{name}') )
+    savefig( os.path.join(get_plot_folder_path(), name) )
 
     if SHOW_PLOTS:
         show()
 
 def plot_correlation(data, name):
+
+    fig = figure(figsize=[12, 12])
+    corr_mtx = abs(data.corr())
+    heatmap(abs(corr_mtx), xticklabels=corr_mtx.columns, yticklabels=corr_mtx.columns, annot=True, cmap='Blues')
+    title('Correlation analysis')
     savefig( os.path.join(get_plot_folder_path(), name) )
 
     if SHOW_PLOTS:
@@ -50,3 +59,8 @@ if __name__ == "__main__":
 
     data_diabetic, data_drought = read_data()
 
+    plot_scatter(data_diabetic, 'spars_diabetic_scatter')
+    plot_scatter(data_drought, 'spars_drought_scatter')
+
+    plot_correlation(data_diabetic, 'spars_diabetic_corr')
+    plot_correlation(data_diabetic, 'spars_drought_corr')
