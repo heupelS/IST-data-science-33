f4f6c1faba3b051327f08a5807325bda4db9081b
nico-koltermann <nico.koltermann@get-racing.de>
2022-12-05 15:22:48 +0000

    Add set2_outliers

---
+++ src/scripts/2_data_preperation/set2_outlier.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys, os
+
+from pandas import DataFrame
+import pandas as pd
+
+sys.path.append( os.path.join(os.path.dirname(__file__), '..', '..','utils') )
+from load_data import read_data, save_new_csv
+
+from outlier_handling import replace_outliers, drop_outliers, truncate_outliers
+
+
+def handle_outliers(data):
+    drop_outliers(data.copy(), 'drought_drop_outliers.csv')
+    replace_outliers(data.copy(), 'drought_replace_outliers.csv')
+    truncate_outliers(data.copy(), 'drought_truncate_outliers.csv')
+
+
+if __name__ == "__main__":
+
+    _, data_drought = read_data()
+
+    # Encoding
+    handle_outliers(data_drought)
