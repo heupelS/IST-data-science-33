71a805e4b0d0afca7f5bb39a95d16fe10eff2c2c
nico-koltermann <nico.koltermann@get-racing.de>
2022-12-05 14:26:00 +0000

    Set2 encoding finished

--- src/scripts/2_data_preperation/set2_encoding.py
+++ src/scripts/2_data_preperation/set2_encoding.py
@@ -9,29 +9,41 @@ sys.path.append( os.path.join(os.path.dirname(__file__), '..', '..','utils') )
 from general_utils import get_plot_folder_path
 from load_data import read_data
 from knn import KNN
+from naive_bayes import NB
 
 from ds_charts import get_variable_types
 from matplotlib.pyplot import savefig, show, figure
+
+
+###########################################
+## Select if plots show up of just saved ##
+SHOW_PLOTS = False
+###########################################
 
 
 def encode_time(df):
     # Transform to unix timestamp
     df['date'] = pd.to_datetime(df['date']).astype(int) / 10**9
 
+
 def evaluate_drought(df):
     df['drought'] = df['class']
-    df.drop('class')
+    df.drop(['class'], axis=1)
+
 
 if __name__ == "__main__":
 
     _, data_drought = read_data()
 
+    # Encoding
     encode_time(data_drought)
+    evaluate_drought(data_drought)
 
-    # evaluate_drought(data_drought)
-
+    #  [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
     nvalues = [17]
+    # ['manhattan', 'euclidean', 'chebyshev']
     dist = ['manhattan']
-    KNN(data_drought, 'class', 'drought_knn_best_res', nvalues=nvalues, dist=dist)
 
-
+    # Evaluation
+    NB(data_drought.copy(), 'drought', 'drought_nb_best_res')
+    KNN(data_drought.copy(), 'drought', 'drought_knn_best_res', nvalues=nvalues, dist=dist)
