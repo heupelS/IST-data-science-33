aaabcd5b60b8546e3fe87d3005ce6f57dfef294e
nico-koltermann <nico.koltermann@get-racing.de>
2022-12-05 19:05:47 +0000

    set 2 outlier

--- src/scripts/2_data_preperation/set2_scaling.py
+++ src/scripts/2_data_preperation/set2_scaling.py
@@ -7,21 +7,27 @@ import pandas as pd
 sys.path.append( os.path.join(os.path.dirname(__file__), '..', '..','utils') )
 from load_data import read_data_by_filename, save_new_csv
 
+from scaling import std_scaler_z_score, std_scaler_z_minmax, scale_boxplot
+
 ###########################################
 ## Select if plots show up of just saved ##
-FILENAME = 'drought_truncate_outliers.csv'
+FILENAME = 'drought_drop_outliers.csv'
 ###########################################
 
 
 def scaling(df):
-    pass
+    zscore = std_scaler_z_score(df, 'std_scaler_z_score.csv')
+    minmax = std_scaler_z_minmax(df, 'std_scaler_z_minmax.csv')
+
+    scale_boxplot(df, zscore, minmax, 'std_scaler_comparison', True)
 
 
 if __name__ == "__main__":
 
     data_drought = read_data_by_filename(FILENAME)
 
-    print(data_drought.shape)
+    data_drought.drop(columns=['Unnamed: 0'], inplace=True)
+    data_drought.drop(columns=['Unnamed: 0.1'], inplace=True)
 
-    # Encoding
+    # Scaling encoded data with handled missing values and outliers
     scaling(data_drought)
