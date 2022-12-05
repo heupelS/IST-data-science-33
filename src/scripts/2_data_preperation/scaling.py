7165f04b55b97d5aa1969818ccfb386e063dc3a2
nico-koltermann <nico.koltermann@get-racing.de>
2022-12-05 19:06:45 +0000

    Finish scaling functions

--- src/scripts/2_data_preperation/scaling.py
+++ src/scripts/2_data_preperation/scaling.py
@@ -1,22 +1,54 @@
+import sys, os
+
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from pandas import DataFrame, concat
 
 sys.path.append( os.path.join(os.path.dirname(__file__), '..', '..','utils') )
+from general_utils import get_plot_folder_path
 from load_data import save_new_csv
+
+from ds_charts import get_variable_types
+
+from matplotlib.pyplot import subplots, show, savefig
 
 
 def std_scaler_z_score(df, filename):
+
+    print(df.info())
+
     transf = StandardScaler(with_mean=True, with_std=True, copy=True).fit(df)
-    norm_data_zscore = transf.transform(df)
+    norm_data_zscore = DataFrame(transf.transform(df))
 
     save_new_csv(norm_data_zscore, filename)
     print(norm_data_zscore.describe())
 
+    return norm_data_zscore
 
-def std_scaler_z_score(df, filename):
+def std_scaler_z_minmax(df, filename):
     transf = MinMaxScaler(feature_range=(0, 1), copy=True).fit(df)
-    norm_data_minmax = transf.transform(df_nr)
+    norm_data_minmax = DataFrame(transf.transform(df))
     
     save_new_csv(norm_data_minmax, filename)
     print(norm_data_minmax.describe())
+
+    return norm_data_minmax
+
+
+def scale_boxplot(df, df_zscore, df_minmax, filename, showplot=False):
+    
+    fig, axs = subplots(1, 3, figsize=(20,10),squeeze=False)
+    
+    axs[0, 0].set_title('Original data')
+    df.boxplot(ax=axs[0, 0])
+    
+    axs[0, 1].set_title('Z-score normalization')
+    df_zscore.boxplot(ax=axs[0, 1])
+    
+    axs[0, 2].set_title('MinMax normalization')
+    df_minmax.boxplot(ax=axs[0, 2])
+    
+    savefig( os.path.join(get_plot_folder_path(), filename) )
+
+    if showplot:
+        show()
 
