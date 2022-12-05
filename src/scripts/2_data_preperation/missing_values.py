b075481849d347a04b933ba6dceb12e36faf6ad8
Simeon <simeon@heupel.de>
2022-12-05 21:48:16 +0000

    fixed na functions

--- src/scripts/2_data_preperation/missing_values.py
+++ src/scripts/2_data_preperation/missing_values.py
@@ -1,6 +1,7 @@
 from sklearn.impute import SimpleImputer
 from pandas import concat, DataFrame
 from numpy import nan
+import sys, os
 
 sys.path.append( os.path.join(os.path.dirname(__file__), '..', '..','utils') )
 from load_data import save_new_csv
@@ -10,6 +11,15 @@ from ds_charts import get_variable_types
 
 # Data should be encoded and inlcude only numeric variables!
 # strategy: mean / median 
+
+
+def get_na_distribution_dict(df):
+    mv = {}
+    for var in df:
+        nr = df[var].isna().sum()
+        if nr > 0:
+            mv[var] = nr
+
 def filling_missing_value(df, filename, strategy):
 
     imp = SimpleImputer(strategy=strategy, fill_value=False, missing_values=nan, copy=True)
@@ -21,9 +31,9 @@ def filling_missing_value(df, filename, strategy):
 
 def drop_missing_records(df, filename):
     # defines the number of variables to discard entire records
-    threshold = data.shape[1] * 0.50
+    threshold = df.shape[1] * 0.50
 
-    df = data.dropna(thresh=threshold, inplace=False)
+    df = df.dropna(thresh=threshold, inplace=False)
 
     save_new_csv(df, filename)
     print(df.shape)
@@ -31,10 +41,10 @@ def drop_missing_records(df, filename):
 
 def drop_missing_values_cols(df, filename):
     # defines the number of records to discard entire columns
-    threshold = data.shape[0] * 0.90
-
+    threshold = df.shape[0] * 0.90
+    mv = get_na_distribution_dict(df)
     missings = [c for c in mv.keys() if mv[c]>threshold]
-    df = data.drop(columns=missings, inplace=False)
+    df = df.drop(columns=missings, inplace=False)
 
     save_new_csv(df, filename)
     print('Dropped variables', missings)
