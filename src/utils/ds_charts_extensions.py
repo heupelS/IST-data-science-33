da9a24386e02a9f8b2018f9b223deac115be3721
Simeon <simeon@heupel.de>
2022-12-08 18:32:42 +0000

    made function smaller

--- src/utils/ds_charts_extensions.py
+++ src/utils/ds_charts_extensions.py
@@ -7,16 +7,10 @@ from matplotlib.pyplot import Axes, gca, figure, savefig, subplots, imshow, imre
 
 def plot_evaluation_results_multi_label(labels: ndarray, trn_y, prd_trn, tst_y, prd_tst):
     cnf_mtx_trn = confusion_matrix(trn_y, prd_trn, labels=labels)
-    fp_trn = np.sum(cnf_mtx_trn.sum(axis=0) - np.diag(cnf_mtx_trn)    )
-    fn_trn = np.sum(cnf_mtx_trn.sum(axis=1) - np.diag(cnf_mtx_trn)    )
-    tp_trn = np.sum(np.diag(cnf_mtx_trn)   )
-    tn_trn = np.sum(np.sum(cnf_mtx_trn) - (fp_trn + fn_trn + tp_trn))
+    fp_trn,fn_trn,tp_trn,tn_trn = cnf_result_multilabel(cnf_mtx_trn)
 
     cnf_mtx_tst = confusion_matrix(tst_y, prd_tst, labels=labels)
-    fp_tst = np.sum(np.sum(cnf_mtx_tst, axis=0) - np.diag(cnf_mtx_tst) )
-    fn_tst = np.sum(np.sum(cnf_mtx_tst, axis=1) - np.diag(cnf_mtx_tst) )
-    tp_tst = np.sum(np.diag(cnf_mtx_tst) ) 
-    tn_tst = np.sum(np.sum(cnf_mtx_tst) - (fp_tst + fn_tst + tp_tst))
+    fp_tst,fn_tst,tp_tst,tn_tst = cnf_result_multilabel(cnf_mtx_tst)
 
     evaluation = {
         'Accuracy': [(tn_trn + tp_trn) / (tn_trn + tp_trn + fp_trn + fn_trn), (tn_tst + tp_tst) / (tn_tst + tp_tst + fp_tst + fn_tst)],
@@ -26,3 +20,10 @@ def plot_evaluation_results_multi_label(labels: ndarray, trn_y, prd_trn, tst_y, 
 
     _, axs = subplots(1, 2, figsize=(2 * HEIGHT, HEIGHT))
     plot_confusion_matrix(cnf_mtx_tst, labels, ax=axs[1], title='Test')
+
+def cnf_result_multilabel(cnf_mtx_trn):
+    fp = np.sum(cnf_mtx_trn.sum(axis=0) - np.diag(cnf_mtx_trn)    )
+    fn = np.sum(cnf_mtx_trn.sum(axis=1) - np.diag(cnf_mtx_trn)    )
+    tp = np.sum(np.diag(cnf_mtx_trn)   )
+    tn = np.sum(np.sum(cnf_mtx_trn) - (fp + fn + tp))
+    return fp,fn,tp,tn
