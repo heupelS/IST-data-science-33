d7a337ecd1bd0ea0f4ae164c5856d5186751174d
Natan Viana <63097790+NatanViana@users.noreply.github.com>
2022-12-11 21:08:01 +0000

    correction of parameters

--- src/scripts/2_data_preperation/set2_balancing.py
+++ src/scripts/2_data_preperation/set2_balancing.py
@@ -13,7 +13,6 @@ from load_data import read_data_by_filename
 ## Select if plots show up or just saved ##
 FILENAME = 'drought_replace_outliers_std_scaler_z_minmax.csv'
 filename = FILENAME.split("_")[0]+'_dataset'
-RUN_EVALUATION = False
 ###########################################
 
 
@@ -24,8 +23,8 @@ def balancing_dataset(data, target):
     # I think for this dataset is the better approach because we have 60000 records and the data is close to be total balanced. 29382 vs 30047
     undersample_dataset(data, target, filename)
 
-    #oversample_dataset(diabeteic_data, 'readmitted', 'diabetic_dataset')
-    #smote_dataset(diabeteic_data, 'readmitted', 'diabetic_dataset')
+    #oversample_dataset(data, target, filename)
+    #smote_dataset(data, target, filename)
 
 
 if __name__ == "__main__":
