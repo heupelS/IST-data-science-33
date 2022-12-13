26c5be44580afe7ea27e7647eebbec400d785e02
Natan Viana <63097790+NatanViana@users.noreply.github.com>
2022-12-13 15:12:44 +0000

    Data Balancing working for multilabels
    Now the data balancing is working for both sets.

--- src/utils/data_preperation/data_balancing.py
+++ src/utils/data_preperation/data_balancing.py
@@ -28,44 +28,54 @@ RANDOM_STATE = 42
 def plot_dataset_balance(df: DataFrame, target_var: str, save_file_name: str):
     class_var = target_var
     target_count = df[class_var].value_counts()
-    positive_class = target_count.idxmin()
-    negative_class = target_count.idxmax()
+    print("Target Count:",target_count)
+
+    majority_class = target_count.idxmax()
+    #print("Majority_class:", majority_class)
     #ind_positive_class = target_count.index.get_loc(positive_class)
-    print('Minority class=', positive_class, ':', target_count[positive_class])
-    print('Majority class=', negative_class, ':', target_count[negative_class])
-    print('Proportion:', round(target_count[positive_class] / target_count[negative_class], 2), ': 1')
-    values = {'Original': [target_count[positive_class], target_count[negative_class]]}
+
+    values = {'Original': []}
+    for index in target_count.index:
+        
+        print('class=', index, ':', target_count[index])
+        print('Proportion:', round(target_count[index] / target_count[majority_class], 2), ': 1')
+        values['Original'].append(target_count[index])
+    
+    #print(values)
 
     figure()
     bar_chart(target_count.index, target_count.values, title='Class balance')
     savefig(os.path.join(get_plot_folder_path(), '%s_Class_balance' % save_file_name) )
+    print("------------- FINAL BALANCE -------------------")
     
     if SHOW_PLOTS:
         show()
-
-
-
 
 def undersample_dataset(df: DataFrame, target_var: str, new_file_name: str):
     class_var = target_var
 
     target_count = df[class_var].value_counts()
-    positive_class = target_count.idxmin()
-    negative_class = target_count.idxmax()
 
-    df_positives = df[df[class_var] == positive_class]
-    df_negatives = df[df[class_var] == negative_class]
+    minority_class = target_count.idxmin()
 
-    values = {'Original': [target_count[positive_class], target_count[negative_class]]}
-    df_neg_sample = DataFrame(df_negatives.sample(len(df_positives)))
-    df_under = concat([df_positives, df_neg_sample], axis=0)
+    values = {'Original': []}
+    df_simple = {}
+    for index in target_count.index:
+        values['Original'].append(target_count[index])
+        df_simple[index] = df[df[class_var] == index]
+
+    df_neg_sample= {}
+    values['UnderSample'] = []
+    for index in target_count.index:
+        df_neg_sample[index] = DataFrame(df_simple[index].sample(target_count[minority_class]))
+        values['UnderSample'].append(len(df_neg_sample[index]))
+        #print('Proportion:', round(target_count[minority_class] / len(df_neg_sample[index]), 2), ': 1')
+    
+    df_under = pd.concat(df_neg_sample.values(), ignore_index=True)
 
     save_new_csv(df_under, '%s_undersampled.csv' % new_file_name)
-
-    values['UnderSample'] = [len(df_positives), len(df_neg_sample)]
-    print('Minority class=', positive_class, ':', len(df_positives))
-    print('Majority class=', negative_class, ':', len(df_neg_sample))
-    print('Proportion:', round(len(df_positives) / len(df_neg_sample), 2), ': 1')
+    print("Original dataset shape:",df.shape)
+    print("New dataset shape:",df_under.shape)
 
     plot_dataset_balance(df_under, target_var, '%s_undersampled'% new_file_name)
 
@@ -74,49 +84,63 @@ def oversample_dataset(df: DataFrame, target_var: str, new_file_name: str):
     class_var = target_var
 
     target_count = df[class_var].value_counts()
-    positive_class = target_count.idxmin()
-    negative_class = target_count.idxmax()
 
-    df_positives = df[df[class_var] == positive_class]
-    df_negatives = df[df[class_var] == negative_class]
+    majority_class = target_count.idxmax()
 
-    values = {'Original': [target_count[positive_class], target_count[negative_class]]}
-    df_pos_sample = DataFrame(df_positives.sample(len(df_negatives), replace=True))
-    df_over = concat([df_pos_sample, df_negatives], axis=0)
+    values = {'Original': []}
+    df_simple = {}
+    for index in target_count.index:
+        values['Original'].append(target_count[index])
+        df_simple[index] = df[df[class_var] == index]
 
+    df_pos_sample= {}
+    values['OverSample'] = []
+    for index in target_count.index:
+        df_pos_sample[index] = DataFrame(df_simple[index].sample(target_count[majority_class], replace = True))
+        values['OverSample'].append(len(df_pos_sample[index]))
+        #print('Proportion:', round(target_count[majority_class] / len(df_pos_sample[index]), 2), ': 1')
+    
+    df_over = pd.concat(df_pos_sample.values(), ignore_index=True)
+
+    print("Original dataset shape:",df.shape)
+    print("New dataset shape:",df_over.shape)
+   
     save_new_csv(df_over, '%s_oversampled.csv' % new_file_name)
 
-    values['OverSample'] = [len(df_pos_sample), len(df_negatives)]
-    print('Minority class=', positive_class, ':', len(df_pos_sample))
-    print('Majority class=', negative_class, ':', len(df_negatives))
-    print('Proportion:', round(len(df_pos_sample) / len(df_negatives), 2), ': 1')
+    plot_dataset_balance(df_over, target_var, '%s_oversampled'% new_file_name)
+
 
 def smote_dataset(df: DataFrame, target_var: str, new_file_name: str):
     class_var = target_var
 
     target_count = df[class_var].value_counts()
-    positive_class = target_count.idxmin()
-    negative_class = target_count.idxmax()
 
-    df_positives = df[df[class_var] == positive_class]
-    df_negatives = df[df[class_var] == negative_class]
+    majority_class = target_count.idxmax()
 
-    values = {'Original': [target_count[positive_class], target_count[negative_class]]}
+    values = {'Original': []}
+    df_simple = {}
+    for index in target_count.index:
+        values['Original'].append(target_count[index])
+        df_simple[index] = df[df[class_var] == index]
     
-    smote = SMOTE(sampling_strategy='minority', random_state=RANDOM_STATE)
-    y = original.pop(class_var).values
-    X = original.values
+    
+    smote = SMOTE(sampling_strategy='not majority', random_state=RANDOM_STATE)
+    y = df.pop(class_var).values
+    X = df.values
     smote_X, smote_y = smote.fit_resample(X, y)
     df_smote = concat([DataFrame(smote_X), DataFrame(smote_y)], axis=1)
-    df_smote.columns = list(original.columns) + [class_var]
+    df_smote.columns = list(df.columns) + [class_var]
+
+    print("Original dataset shape:",df.shape)
+    print("New dataset shape:",df_smote.shape)
 
     save_new_csv(df_smote, '%s_smoted.csv' % new_file_name)
 
     smote_target_count = Series(smote_y).value_counts()
 
-    values['SMOTE'] = [smote_target_count[positive_class], smote_target_count[negative_class]]
-    print('Minority class=', positive_class, ':', smote_target_count[positive_class])
-    print('Majority class=', negative_class, ':', smote_target_count[negative_class])
-    print('Proportion:', round(smote_target_count[positive_class] / smote_target_count[negative_class], 2), ': 1')
+    values = {'SMOTE': []}
+    for index in smote_target_count.index:
+        values['SMOTE'].append(target_count[index])
 
+    plot_dataset_balance(df_smote, target_var, '%s_smoted'% new_file_name)
 
