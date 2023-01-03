6b4675d8f95d16dd4b74464cbbb0d83e6bd87f55
nico-koltermann <nico.koltermann@get-racing.de>
2023-01-03 13:44:43 +0000

    Add changes to set2 file

--- src/scripts/6_time_series/set2_prep.py
+++ src/scripts/6_time_series/set2_prep.py
@@ -6,7 +6,7 @@ from load_data import read_time_series_by_filename, save_new_csv
 from data_profiling.granuality import data_granularity
 
 from time_series.ts_profiling import box_plot, var_distribution, data_stationary
-from time_series.ts_transformation import smoothing, differention, aggregate_multi
+from time_series.ts_transformation import plot_smoothing, plot_differention, plot_aggregate_multi
 from time_series.ts_forecasting import forecast
 
 def final_set_forecasting_drought():
@@ -16,7 +16,7 @@ def final_set_forecasting_drought():
     return data_set2_sorted
 
 
-def set1_profiling(data_set2):
+def set2_profiling(data_set2):
 
     data_granularity(data_set2, 'drought_gran', 'Numeric')
 
@@ -24,7 +24,7 @@ def set1_profiling(data_set2):
     agg_types = ['S', 'H', 'D', 'W', 'M', 'Q']
 
     for agg in agg_types:
-        aggregate_multi(
+        plot_aggregate_multi(
             data=data_set2, 
             agg_type=agg, 
             index_multi='date', 
@@ -37,18 +37,18 @@ def set1_profiling(data_set2):
     data_stationary(data_set2, 'QV2M', 'drought')
 
 
-def set1_transformation(data_set2):
+def set2_transformation(data_set2):
     targets = ['PRECTOT', 'PS', 'T2M', 'T2MDEW', 'T2MWET', 'TS', 'QV2M']
 
-    smoothing(data_set2.copy(), targets, 'glucose', 100, 'Date', 'Consumption')
-    differention(data_set2.copy(), targets, 'glucose', 100, 'Date', 'Consumption')
+    plot_smoothing(data_set2.copy(), targets, 'glucose', 100, 'Date', 'Consumption')
+    plot_differention(data_set2.copy(), targets, 'glucose', 100, 'Date', 'Consumption')
 
 
 if __name__ == '__main__':
     data_set2 = final_set_forecasting_drought()
 
-    set1_profiling(data_set2)
-    set1_transformation(data_set2)
+    set2_profiling(data_set2)
+    set2_transformation(data_set2)
    
 
 
