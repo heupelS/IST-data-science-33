a8f68717eb4135612cc6e510450105aae13bf59a
nico-koltermann <nico.koltermann@get-racing.de>
2023-01-18 12:47:22 +0000

    Add set set1 to final

--- src/scripts/2_data_preperation/create_final_datasets.py
+++ src/scripts/2_data_preperation/create_final_datasets.py
@@ -3,8 +3,25 @@ sys.path.append( os.path.join(os.path.dirname(__file__), '..', '..','utils') )
 from load_data import read_data
 from data_preperation.outlier_handling import drop_outliers
 from set2_encoding import encode_time, evaluate_drought
-from data_preperation.scaling import std_scaler_minmax
+from data_preperation.scaling import std_scaler_minmax, std_scaler_z_score
+from set1_encoding_evaluate import *
 
+
+def final_set1():
+    data, _ = read_data()
+
+    data_diabetic = drop_id_cols(data)
+
+    data_diabetic = replace_questionmarks(data_diabetic)
+
+    data_diabetic_mv_deleted_rows = drop_missing_records(data_diabetic.copy(), 'data_diabetic_mv_deleted_rows.csv') 
+
+    final_df = encode(data_diabetic_mv_deleted_rows, 'diabetic_mv_deleted_rows_encoded')
+
+    drop_outliers(final_df, 'diabetic_drop_outliers.csv')
+
+    std_scaler_minmax(final_df.copy(), 'readmitted', 'diabetic_minmax.csv')
+    std_scaler_z_score(final_df.copy(), 'readmitted', 'diabetic_zscore.csv')
 
 def final_set2():
     _, data_drought = read_data()
@@ -13,9 +30,9 @@ def final_set2():
     encode_time(data_drought)
     evaluate_drought(data_drought)
 
-    data_drought = drop_outliers(data_drought.copy(), 'drought_drop_outliers.csv')
+    drop_outliers(data_drought, 'drought_drop_outliers.csv')
 
-    data_drought = std_scaler_minmax(data_drought.copy(), 'drought', 'drought_prep.csv')
+    std_scaler_minmax(data_drought, 'drought', 'drought_prep.csv')
 
 if __name__ == '__main__':
-    final_set2()
+    final_set1()
